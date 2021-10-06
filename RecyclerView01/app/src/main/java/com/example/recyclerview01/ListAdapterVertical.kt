fa9159ee5d7360403118028b01d1dd9a3c283d62
Notepad96 <notepad1029@gmail.com>
2021-10-06 13:10:06 +0900

    example success

--- RecyclerView01/app/src/main/java/com/example/recyclerview01/ListAdapterVertical.kt
+++ RecyclerView01/app/src/main/java/com/example/recyclerview01/ListAdapterVertical.kt
@@ -7,8 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item_vert.view.*
 
 class ListAdapterVertical:  RecyclerView.Adapter<ListAdapterVertical.ListAdapter>(){
-    var titles = arrayListOf("Title 1", "Title 2", "Title 3", "Title 4")
-    var contents = arrayListOf("Content 1", "Content 2", "Content 3", "Content 4")
+    var titles = arrayListOf("Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8")
+    var contents = arrayListOf("Content 1", "Content 2", "Content 3", "Content 4", "Content 5", "Content 6", "Content 7", "Content 8")
 
     class ListAdapter(val layout: View): RecyclerView.ViewHolder(layout)
 
