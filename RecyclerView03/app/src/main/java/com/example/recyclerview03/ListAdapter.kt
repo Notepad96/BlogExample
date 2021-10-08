c0f2106b89b19a8c060731dcc3bbeb8cd41b92b7
Notepad96 <notepad1029@gmail.com>
2021-10-08 16:18:38 +0900

    end

--- RecyclerView03/app/src/main/java/com/example/recyclerview03/ListAdapter.kt
+++ RecyclerView03/app/src/main/java/com/example/recyclerview03/ListAdapter.kt
@@ -3,28 +3,60 @@ package com.example.recyclerview03
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.list_footer.view.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class ListAdapter(var list: ArrayList<String>): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+    val HEADER = 1
+    val ITEM = 2
+    val FOOTER = 3
 
     class ListViewHolder(val layout: View): RecyclerView.ViewHolder(layout)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         var view =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            when(viewType) {
+                HEADER ->
+                    LayoutInflater.from(parent.context).inflate(R.layout.list_header, parent, false)
+                FOOTER ->
+                    LayoutInflater.from(parent.context).inflate(R.layout.list_footer, parent, false)
+                else ->
+                    LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            }
 
         return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-
+        if(position == 0) {
+            // HEADER
+        } else if(position == list.size + 1) {
+            // FOOTER
+            holder.layout.layoutListFooter.setOnClickListener {
+                list.add("")
+                this.notifyDataSetChanged()
+            }
+        } else {
+            // ITEM
+            holder.layout.textListTitle.text = "Title $position"
+            holder.layout.textListContent.text = "Contents $position"
+        }
     }
 
     override fun getItemCount(): Int {
-        return list.size + 3
+        return list.size + 2
     }
 
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
+        return if(position == 0) {
+            HEADER
+        } else if(position == list.size + 1) {
+            FOOTER
+        } else {
+            ITEM
+        }
     }
 }
--- RecyclerView03/app/src/main/java/com/example/recyclerview03/MainActivity.kt
+++ RecyclerView03/app/src/main/java/com/example/recyclerview03/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.recyclerview03
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var list = arrayListOf<String>()
         var listManager = LinearLayoutManager(this)
-        var listAdapter = ListAdapter()
+        var listAdapter = ListAdapter(list)
 
+        var recyclerView = recyclerList.apply {
+            setHasFixedSize(true)
+            layoutManager = listManager
+            adapter = listAdapter
+        }
     }
 }
