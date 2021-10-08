192a859787820c8a2a6608ffffda4e0ad9be2a14
Notepad96 <notepad1029@gmail.com>
2021-10-08 12:36:21 +0900

    end

---
+++ RecyclerView02/app/src/main/java/com/example/recyclerview02/ListAdapterGrid.kt
@@ -0,0 +1,26 @@
+package com.example.recyclerview02
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_grid_item.view.*
+
+class ListAdapterGrid(var list: ArrayList<String>): RecyclerView.Adapter<ListAdapterGrid.GridAdapter>() {
+
+    class GridAdapter(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridAdapter {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_grid_item, parent, false)
+
+        return GridAdapter(view)
+    }
+
+    override fun onBindViewHolder(holder: GridAdapter, position: Int) {
+        holder.layout.textListTitle.text = list[position]
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- RecyclerView02/app/src/main/java/com/example/recyclerview02/MainActivity.kt
+++ RecyclerView02/app/src/main/java/com/example/recyclerview02/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.recyclerview02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var list = arrayListOf("Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8")
+        var listManager = GridLayoutManager(this, 3)
+        var listAdapter = ListAdapterGrid(list)
+
+        var recyclerList = recyclerGridView.apply {
+            setHasFixedSize(true)
+            layoutManager = listManager
+            adapter = listAdapter
+        }
     }
 }
