3396a88977882b700feaf2e070d856d5c8d71f7c
Notepad96 <notepad1029@gmail.com>
2021-10-09 21:09:55 +0900

    preview add

--- ClickEvent/app/src/main/java/com/example/clickevent/MainActivity.kt
+++ ClickEvent/app/src/main/java/com/example/clickevent/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.clickevent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
