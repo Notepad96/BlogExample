7191af553cf20a12417638016be376080fa2bdce
Notepad96 <notepad1029@gmail.com>
2021-12-18 10:53:42 +0900

    preview upload

--- ButtonStyleCode/app/src/main/java/com/example/buttonstylecode/MainActivity.kt
+++ ButtonStyleCode/app/src/main/java/com/example/buttonstylecode/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.buttonstylecode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ContextThemeWrapper
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var num = 0
+
+        vBtnAddDefault.setOnClickListener {
+            var btn = Button(applicationContext)
+            btn.text = "Button ${++num}"
+            vLayoutAddBtn.addView(btn)
+        }
+
+        vBtnAddCustom.setOnClickListener {
+            var newStyle = ContextThemeWrapper(this, R.style.Button_Border)
+            var btn = MaterialButton(newStyle)
+            btn.text = "Button ${++num}"
+            vLayoutAddBtn.addView(btn)
+        }
     }
 }
