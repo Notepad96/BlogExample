06604d4cdb785be9a2a77d5fe403184d1c73cc82
Notepad96 <notepad1029@gmail.com>
2021-12-19 12:02:59 +0900

    example end

--- SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,17 +1,55 @@
 package com.example.sharedpreferences
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var option: SharedPreferences
+    lateinit var userInfo: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        option = getSharedPreferences("option", MODE_PRIVATE)
+        userInfo = getSharedPreferences("user_info", MODE_PRIVATE)
+
+
+
         vBtnMoveActivity.setOnClickListener {
             var intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
+
+        vBtnNameSave.setOnClickListener {
+            userInfo.edit {
+                putString("name", vEditUserName.text.toString())
+                vEditUserName.setText("")
+            }
+            Toast.makeText(applicationContext, "Name Save!!", Toast.LENGTH_LONG).show()
+        }
+
+        vSwitchAlarm.setOnCheckedChangeListener { compoundButton, b ->
+            option.edit {
+                putBoolean("alarm", vSwitchAlarm.isChecked)
+            }
+        }
+    }
+
+    override fun onStart() {
+        saveValueLoad() // Value Load
+        super.onStart()
+    }
+
+    private fun saveValueLoad() {
+        vEditUserName.setText(userInfo.getString("name", ""))
+
+        vSwitchAlarm.isChecked = option.getBoolean("alarm", false)
     }
 }
--- SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity2.kt
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity2.kt
@@ -1,11 +1,33 @@
 package com.example.sharedpreferences
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var option: SharedPreferences
+    lateinit var userInfo: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        option = getSharedPreferences("option", MODE_PRIVATE)
+        userInfo = getSharedPreferences("user_info", MODE_PRIVATE)
+
+        var str = """User Name: ${userInfo.getString("name", "NULL")}
+            |
+            |Alarm On/Off: ${boolToText(option.getBoolean("alarm", false))}
+        """.trimMargin()
+
+        vTextResult.text = str
+    }
+
+    fun boolToText(check: Boolean): String {
+        return when(check) {
+            true -> "On"
+            else -> "Off"
+        }
     }
 }
