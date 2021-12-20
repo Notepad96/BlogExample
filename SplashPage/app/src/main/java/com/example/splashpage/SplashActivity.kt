81fbf06f06fd28acb3cb90ee2278c5103d730d90
Notepad96 <notepad1029@gmail.com>
2021-12-21 00:01:15 +0900

    preview update

--- SplashPage/app/src/main/java/com/example/splashpage/SplashActivity.kt
+++ SplashPage/app/src/main/java/com/example/splashpage/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.example.splashpage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     private val DELAY_TIME = 1500L
