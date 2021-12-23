97ed18cfea6b3a2e6fbbfd4494b6500c778ccc20
Notepad96 <notepad1029@gmail.com>
2021-12-23 20:58:31 +0900

    example complete

--- MenuOption/app/src/main/java/com/example/menuoption/MainActivity.kt
+++ MenuOption/app/src/main/java/com/example/menuoption/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.menuoption
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        vTextMenuItem.text = item.toString()
+        return super.onOptionsItemSelected(item)
     }
 }
