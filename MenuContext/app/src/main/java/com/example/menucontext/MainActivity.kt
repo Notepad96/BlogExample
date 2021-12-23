c2e773907f624a506b68c20f2486a92c9acc6293
Notepad96 <notepad1029@gmail.com>
2021-12-23 22:46:53 +0900

    example complete

---
+++ MenuContext/app/src/main/java/com/example/menucontext/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.menucontext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        registerForContextMenu(vTextMenuItem)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        vTextMenuItem.text = item.toString()
+        return super.onContextItemSelected(item)
+    }
+}
