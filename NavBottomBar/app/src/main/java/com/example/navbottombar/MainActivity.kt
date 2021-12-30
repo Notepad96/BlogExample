d73cf9a82ec3677961d55540219e6af8ce2abf64
Notepad96 <notepad1029@gmail.com>
2021-12-30 13:48:14 +0900

    clear main layout

--- NavBottomBar/app/src/main/java/com/example/navbottombar/MainActivity.kt
+++ NavBottomBar/app/src/main/java/com/example/navbottombar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.navbottombar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nav_main_bottom.setOnItemSelectedListener {
+
+
+            true
+        }
     }
+
+
 }
