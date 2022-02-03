240a37e5ad9521bed73bc641bad005773c1fa317
Notepad96 <notepad1029@gmail.com>
2022-02-04 02:59:02 +0900

    create example

---
+++ PermissionsExample/app/src/main/java/com/example/permissionsexample/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.permissionsexample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSIONS = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        var permission = mutableMapOf<String, String>()
+        permission["camera"] = Manifest.permission.CAMERA
+        permission["storageRead"] = Manifest.permission.READ_EXTERNAL_STORAGE
+        permission["storageWrite"] =  Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+        // 현재 권한 상태 검사
+        var denied = permission.count { ContextCompat.checkSelfPermission(this, it.value)  == PackageManager.PERMISSION_DENIED }
+
+        // 마시멜로 버전 이후
+        if(denied > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permission.values.toTypedArray(), REQUEST_PERMISSIONS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_PERMISSIONS) {
+
+            /* 1. 권한 확인이 다 끝난 후 동의하지 않은것이 있다면 종료
+            var count = grantResults.count { it == PackageManager.PERMISSION_DENIED } // 동의 안한 권한의 개수
+            if(count != 0) {
+                Toast.makeText(applicationContext, "서비스의 필요한 권한입니다.\n권한에 동의해주세요.", Toast.LENGTH_SHORT).show()
+                finish()
+            } */
+
+            /* 2. 권한 요청을 거부했다면 안내 메시지 보여주며 앱 종료 */
+            grantResults.forEach {
+                if(it == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(applicationContext, "서비스의 필요한 권한입니다.\n권한에 동의해주세요.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+}
