0ae7854a2889faa975f6c0673515bd970ffaad20
Notepad96 <notepad1029@gmail.com>
2022-02-06 23:03:56 +0900

    complete example

--- NumberPickerDialog/app/src/main/java/com/example/numberpickerdialog/MainActivity.kt
+++ NumberPickerDialog/app/src/main/java/com/example/numberpickerdialog/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.numberpickerdialog
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_num_select.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var selectNum = 0
 
         btn_number_select.setOnClickListener {
-            val dialog =
+            val layout = layoutInflater.inflate(R.layout.dialog_num_select, null)
+            val build = AlertDialog.Builder(it.context).apply {
+                setView(layout)
+            }
+            val dialog = build.create()
+            dialog.show()
+
+            layout.number_picker.minValue = 1
+            layout.number_picker.maxValue = 100
+            if(selectNum != 0) layout.number_picker.value = selectNum
+
+            layout.btn_cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            layout.btn_ok.setOnClickListener {
+                selectNum = layout.number_picker.value
+                btn_number_select.text = "$selectNum 번"
+                dialog.dismiss()
+            }
+
         }
     }
 }
