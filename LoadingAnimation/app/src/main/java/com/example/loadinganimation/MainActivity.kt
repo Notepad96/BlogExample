31c3ffb17e76660f76574b055c59a5da6e9d5dcf
Notepad96 <notepad1029@gmail.com>
2022-02-09 23:48:02 +0900

    create list item example

--- LoadingAnimation/app/src/main/java/com/example/loadinganimation/MainActivity.kt
+++ LoadingAnimation/app/src/main/java/com/example/loadinganimation/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.loadinganimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listManager = LinearLayoutManager(this)
+        val listAdapter = MyListAdapter()
+
+        recycler.apply {
+            layoutManager = listManager
+            adapter = listAdapter
+        }
     }
 }
