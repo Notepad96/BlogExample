629b7955e1d0f3cd98f4af627e479b2619b2ddbe
Notepad96 <notepad1029@gmail.com>
2022-02-10 22:40:41 +0900

    update preview

--- LoadingAnimation/app/src/main/java/com/example/loadinganimation/MainActivity.kt
+++ LoadingAnimation/app/src/main/java/com/example/loadinganimation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.loadinganimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,9 +15,16 @@ class MainActivity : AppCompatActivity() {
         val listManager = LinearLayoutManager(this)
         val listAdapter = MyListAdapter()
 
-        recycler.apply {
-            layoutManager = listManager
-            adapter = listAdapter
-        }
+        // 로딩 창 보여주기
+        contentLoading.show()
+
+        Handler().postDelayed(Runnable {
+            recycler.apply {
+                layoutManager = listManager
+                adapter = listAdapter
+            }
+            contentLoading.hide()   // 로딩 창 감추기
+        }, 4000)    // Delay 4초
+
     }
 }
