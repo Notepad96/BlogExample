fc80cfef351eff792d57e8b0ff7c7f3adb61430a
Notepad96 <notepad1029@gmail.com>
2022-02-27 15:35:27 +0900

    update: img01 load

--- ImageLibCoil/app/src/main/java/com/example/imagelibcoil/MainActivity.kt
+++ ImageLibCoil/app/src/main/java/com/example/imagelibcoil/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.imagelibcoil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import coil.transform.CircleCropTransformation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        img_test01.load(R.drawable.img01) {
+            transformations(CircleCropTransformation())
+        }
     }
 }
