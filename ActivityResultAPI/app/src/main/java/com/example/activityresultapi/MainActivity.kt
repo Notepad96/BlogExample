a17082f99d105c011652b905c13ae2594174888b
Notepad96 <notepad1029@gmail.com>
2022-03-10 20:25:09 +0900

    add: image load. file.

--- ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
+++ ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.activityresultapi
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.activityresultapi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.mainImg.setImageURI(it)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainBtn.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 }
+
