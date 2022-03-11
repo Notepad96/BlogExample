9e44fcec44926508f5b2abd11963e74034fb52c7
Notepad96 <notepad1029@gmail.com>
2022-03-12 01:33:23 +0900

    update: file image load

--- ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
+++ ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.activityresultapi
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.activityresultapi.databinding.ActivityMainBinding
 
 
@@ -20,7 +24,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.mainBtn.setOnClickListener {
+
+            openDialog(this)
+        }
+    }
+
+    private fun openDialog(context: Context) {
+        val dialogLayout = layoutInflater.inflate(R.layout.dialog_select_image, null)
+        val dialogBuild = AlertDialog.Builder(context).apply {
+            setView(dialogLayout)
+        }
+        val dialog = dialogBuild.create().apply { show() }
+
+        val fileAddBtn = dialogLayout.findViewById<Button>(R.id.dialog_btn_file)
+        fileAddBtn.setOnClickListener {
             getContentImage.launch("image/*")
+            dialog.dismiss()
         }
     }
 }
