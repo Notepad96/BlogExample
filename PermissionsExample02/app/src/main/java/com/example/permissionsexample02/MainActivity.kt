03b073912869753868c1ac5e373857668a0619e9
Notepad96 <notepad1029@gmail.com>
2022-03-16 03:21:10 +0900

    add: detail

--- PermissionsExample02/app/src/main/java/com/example/permissionsexample02/MainActivity.kt
+++ PermissionsExample02/app/src/main/java/com/example/permissionsexample02/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    // 권한 요청
     private val requestMultiplePermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
         results.forEach {
             if(!it.value) {
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    // 추가할 권한들, 매니페스트 파일서도 추가 필요
     private val permissionList = arrayOf(
         Manifest.permission.CAMERA,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
