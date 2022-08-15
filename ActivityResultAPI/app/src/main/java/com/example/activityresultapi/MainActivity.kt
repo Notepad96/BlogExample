2c26db2bb617b570f38317439a8a86f14fee49a4
Notepad96 <notepad1029@gmail.com>
2022-08-15 23:37:09 +0900

    add annotation

--- ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
+++ ActivityResultAPI/app/src/main/java/com/example/activityresultapi/MainActivity.kt
@@ -19,19 +19,31 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    // 파일 불러오기
     private val getContentImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         uri.let { binding.mainImg.setImageURI(uri) }
     }
+
+    // 카메라를 실행한 후 찍은 사진을 저장
     var pictureUri: Uri? = null
     private val getTakePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) {
         if(it) {
             pictureUri.let { binding.mainImg.setImageURI(pictureUri) }
         }
     }
+
+    // 카메라를 실행하며 결과로 비트맵 이미지를 얻음
     private val getTakePicturePreview = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
         bitmap.let { binding.mainImg.setImageBitmap(bitmap) }
     }
 
+    // 요청하고자 하는 권한들
+    private val permissionList = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    // 권한을 허용하도록 요청
     private val requestMultiplePermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
         results.forEach {
             if(!it.value) {
@@ -40,6 +52,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -50,10 +63,7 @@ class MainActivity : AppCompatActivity() {
             openDialog(this)
         }
     }
-    private val permissionList = arrayOf(
-        Manifest.permission.CAMERA,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE)
+
 
     private fun openDialog(context: Context) {
         val dialogLayout = layoutInflater.inflate(R.layout.dialog_select_image, null)
@@ -66,9 +76,13 @@ class MainActivity : AppCompatActivity() {
         val fileAddBtn = dialogLayout.findViewById<Button>(R.id.dialog_btn_file)
 
         cameraAddBtn.setOnClickListener {
+            // 1. TakePicture
             pictureUri = createImageFile()
-//            getTakePicture.launch(pictureUri)   // Require Uri
-            getTakePicturePreview.launch(null)    // Bitmap get
+            getTakePicture.launch(pictureUri)   // Require Uri
+
+            // 2. TakePicturePreview
+//            getTakePicturePreview.launch(null)    // Bitmap get
+
             dialog.dismiss()
         }
         fileAddBtn.setOnClickListener {
